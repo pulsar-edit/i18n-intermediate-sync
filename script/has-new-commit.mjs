5ec170029f6ef268b700f0bcf24c04a2db3fdd0f
Meadowsys <blazeykirin@gmail.com>
2023-07-03 13:26:18 -0700

    fix linking to pulsar commit, make message matching more precise

--- script/has-new-commit.mjs
+++ script/has-new-commit.mjs
@@ -11,5 +11,6 @@ let pulsar = path.resolve("./pulsar");
 let sync_commit_message = cp.execSync("git show -s --format=%B $(git rev-parse HEAD)", { cwd: sync }).toString().trim();
 let pulsar_commit = cp.execSync("git rev-parse HEAD", { cwd: pulsar }).toString().trim();
 
-let has_new = !sync_commit_message.includes(pulsar_commit);
+let has_new = !sync_commit_message.includes(`(automated) updating JSON files from pulsar-edit/pulsar@${pulsar_commit}`);
 console.log(`has-new=${has_new}`);
+console.log(`pulsar-commit=${pulsar_commit}`);
